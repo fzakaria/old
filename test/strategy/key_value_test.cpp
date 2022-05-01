f3df58cd752d1e847e741cdd41f0cefa1968fede
Farid Zakaria <fmzakari@google.com>
2022-05-01 21:19:44 +0000

    Added more tests for key_value

--- test/strategy/key_value_test.cpp
+++ test/strategy/key_value_test.cpp
@@ -24,3 +24,28 @@ TEST(KeyValueStrategyTest, StrictModeTestingSetTrue) {
   ));
   ASSERT_TRUE(strategy.is_strict());
 }
+
+
+TEST(KeyValueStrategyTest, StrictModeTestingSetInvalidString) {
+  auto strategy = KeyValueStrategy(toml::parse(
+    R"(
+      strict = "hi"
+    )"
+  ));
+  ASSERT_FALSE(strategy.is_strict());
+}
+
+TEST(KeyValueStrategyTest, StrictModeTestingSetInvalidNumber) {
+  auto strategy = KeyValueStrategy(toml::parse(
+    R"(
+      strict = 123
+    )"
+  ));
+  ASSERT_TRUE(strategy.is_strict());
+}
+
+TEST(KeyValueStrategyTest, ReplaceEmpty) {
+  auto name = "hello world";
+  auto strategy = KeyValueStrategy(toml::table{});
+  ASSERT_EQ(strategy.resolve(name), "hello world");
+}
