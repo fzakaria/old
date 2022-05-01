41adeb008e1ac6c77e9c4255d909542c18b15a5d
Farid Zakaria <fmzakari@google.com>
2022-05-01 22:36:01 +0000

    Added more tests for key_value and moved to glog for printing

--- src/strategy/key_value.cpp
+++ src/strategy/key_value.cpp
@@ -1,19 +1,20 @@
 #include "key_value.h"
 #include <iostream>
 #include <stdexcept>
+#include "glog/logging.h"
 
 KeyValueStrategy::KeyValueStrategy(toml::v3::table config) : config(config) {
 }
 
 std::string KeyValueStrategy::resolve(std::string_view name) const {
-    if (this->config.contains(name) && this->config[name].is_string()) {
-        std::cerr << "replacing " << name << " with " << this->config[name] << std::endl;
-    } else {
-        std::cerr << "could not find replacement for [" << name << "]" << std::endl;
+    if (!this->config.contains(name) || !this->config[name].is_string()) {
+        LOG(INFO) << "could not find replacement for [" << name << "]";
         if (this->is_strict()) {
             throw std::domain_error("Strict mode enabled, failing startup.");
         }
     }
+
+    LOG(INFO) << "replacing [" << name << "] with [" << this->config[name] << "]";
     return this->config[name].value<std::string>().value_or(std::string{name});
 };
 
--- test/strategy/key_value_test.cpp
+++ test/strategy/key_value_test.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "src/strategy/key_value.h"
 #include "toml++/toml.h"
+#include <stdexcept>
 
 TEST(KeyValueStrategyTest, StrictModeTestingEmpty) {
   auto strategy = KeyValueStrategy(toml::table{});
@@ -48,4 +49,34 @@ TEST(KeyValueStrategyTest, ReplaceEmpty) {
   auto name = "hello world";
   auto strategy = KeyValueStrategy(toml::table{});
   ASSERT_EQ(strategy.resolve(name), "hello world");
+}
+
+TEST(KeyValueStrategyTest, ReplaceEmptyStrict) {
+  auto name = "hello world";
+  auto strategy = KeyValueStrategy(toml::parse(
+    R"(
+      strict = true
+    )"
+  ));
+  EXPECT_THROW(strategy.resolve(name), std::domain_error);
+}
+
+TEST(KeyValueStrategyTest, ReplaceSimple) {
+  auto name = "libruby-2.7.so.2.7";
+  auto strategy = KeyValueStrategy(toml::parse(
+    R"(
+      "libruby-2.7.so.2.7" = "/lib/x86_64-linux-gnu/libruby-2.7.so.2.7"
+    )"
+  ));
+  ASSERT_EQ(strategy.resolve(name), "/lib/x86_64-linux-gnu/libruby-2.7.so.2.7");
+}
+
+TEST(KeyValueStrategyTest, ReplaceWrongType) {
+  auto name = "libruby-2.7.so.2.7";
+  auto strategy = KeyValueStrategy(toml::parse(
+    R"(
+      "libruby-2.7.so.2.7" = 123
+    )"
+  ));
+  ASSERT_EQ(strategy.resolve(name), name);
 }
