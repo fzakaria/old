ce3c92646c06f5987e384c33986b4d2d33d22570
Farid Zakaria <fmzakari@google.com>
2022-07-01 20:17:23 +0000

    Simplified tests

--- test/strategy/key_value_test.cpp
+++ test/strategy/key_value_test.cpp
@@ -7,50 +7,47 @@
 #include "src/config/config.h"
 
 TEST(KeyValueStrategyTest, StrictModeTestingEmpty) {
-  Config config = Config::fromTOML(toml::parse(
+  Config::KeyValue config = Config::KeyValue::fromTOML(toml::parse(
       R"(
     )"));
-  auto strategy = KeyValueStrategy(config.key_value());
+  auto strategy = KeyValueStrategy(config);
   ASSERT_FALSE(strategy.is_strict());
 }
 
 TEST(KeyValueStrategyTest, StrictModeTestingSetFalse) {
-  Config config = Config::fromTOML(toml::parse(
+  Config::KeyValue config = Config::KeyValue::fromTOML(toml::parse(
       R"(
       [key_value]
       strict = false
     )"));
-  auto strategy = KeyValueStrategy(config.key_value());
+  auto strategy = KeyValueStrategy(config);
   ASSERT_FALSE(strategy.is_strict());
 }
 
 TEST(KeyValueStrategyTest, StrictModeTestingSetTrue) {
-  Config config = Config::fromTOML(toml::parse(
+  Config::KeyValue config = Config::KeyValue::fromTOML(toml::parse(
       R"(
-      [key_value]
       strict = true
     )"));
-  auto strategy = KeyValueStrategy(config.key_value());
+  auto strategy = KeyValueStrategy(config);
   ASSERT_TRUE(strategy.is_strict());
 }
 
 TEST(KeyValueStrategyTest, StrictModeTestingSetInvalidString) {
-  Config config = Config::fromTOML(toml::parse(
+  Config::KeyValue config = Config::KeyValue::fromTOML(toml::parse(
       R"(
-      [key_value]
       strict = "hi"
     )"));
-  auto strategy = KeyValueStrategy(config.key_value());
+  auto strategy = KeyValueStrategy(config);
   ASSERT_FALSE(strategy.is_strict());
 }
 
 TEST(KeyValueStrategyTest, StrictModeTestingSetInvalidNumber) {
-  Config config = Config::fromTOML(toml::parse(
+  Config::KeyValue config = Config::KeyValue::fromTOML(toml::parse(
       R"(
-      [key_value]
       strict = 123
     )"));
-  auto strategy = KeyValueStrategy(config.key_value());
+  auto strategy = KeyValueStrategy(config);
   ASSERT_TRUE(strategy.is_strict());
 }
 
@@ -70,20 +67,20 @@ TEST(KeyValueStrategyTest, ReplaceEmptyStrict) {
 
 TEST(KeyValueStrategyTest, ReplaceSimple) {
   auto name = "libruby-2.7.so.2.7";
-  auto config = KeyValueBuilder().with(
-      "libruby-2.7.so.2.7", "/lib/x86_64-linux-gnu/libruby-2.7.so.2.7");
+  Config::KeyValue config = Config::KeyValue::fromTOML(toml::parse(
+      R"(
+      "libruby-2.7.so.2.7" = "/lib/x86_64-linux-gnu/libruby-2.7.so.2.7"
+    )"));
   auto strategy = KeyValueStrategy(config);
   ASSERT_EQ(strategy.resolve(name), "/lib/x86_64-linux-gnu/libruby-2.7.so.2.7");
 }
 
 TEST(KeyValueStrategyTest, ReplaceWrongType) {
   auto name = "libruby-2.7.so.2.7";
-  Config config = Config::fromTOML(toml::parse(
+  Config::KeyValue config = Config::KeyValue::fromTOML(toml::parse(
       R"(
-      [key_value]
       "libruby-2.7.so.2.7" = 123
     )"));
-  std::cout << "Farid: " << config.key_value().is_strict() << std::endl;
-  auto strategy = KeyValueStrategy(config.key_value());
+  auto strategy = KeyValueStrategy(config);
   ASSERT_EQ(strategy.resolve(name), name);
 }
